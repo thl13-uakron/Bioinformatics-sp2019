b55d2176669fe1f1caea2edec1d0550b0815b0be
Minecraft <minecraft@Cindys-MacBook-Pro.local>
2019-02-06 11:36:12 -0500

    added function to create random dna strand

--- project1/project1.py
+++ project1/project1.py
@@ -1,8 +1,19 @@
-"""methods and templates"""
+# libraries
+import random
+import re
 
-def generateStrand(length): """create a random string of dna bases of a set length"""
+#"""methods and templates"""
 
-def isDNA(string): """check if a DNA string contains only valid base characters"""
+"""create a random string of dna bases of a set length"""
+def generateStrand(length):
+    bases = ("a", "t", "c", "g")
+    DNA = ""
+    while len(DNA) < length:
+        DNA = DNA + bases[random.randint(0, 3)]
+    return DNA
+
+"""check if a DNA string contains only valid base characters"""
+def isDNA(string): 
     temp = string.lower()
     temp = temp.replace("a", "")
     temp = temp.replace("g", "")
@@ -10,7 +21,8 @@ def isDNA(string): """check if a DNA string contains only valid base characters"
     temp = temp.replace("t", "")
     return len(temp) == 0
 
-def getComplement(DNA): """get the complementary strand of a DNA string"""
+"""get the complementary strand of a DNA string"""
+def getComplement(DNA): 
     comp = DNA.lower()
     """match bases, use capital letters as temporary values"""
     comp = comp.replace("a", "T")
@@ -20,19 +32,29 @@ def getComplement(DNA): """get the complementary strand of a DNA string"""
     """swap out temporary characters"""
     return comp.lower()
 
-def forwardBind(DNA, length): """return a complementary forward primer strand of a given length"""
+"""return a complementary forward primer strand of a given length"""
+def forwardBind(DNA, length):
+    return
 
-def reverseBind(DNA, length): """return a complementary reverse primer strand of a given length""""
+"""return a complementary reverse primer strand of a given length"""
+def reverseBind(DNA, length):
+    return
 
-def forwardExtend(DNA, primer, length): """extend a forward primer to a given length"""
+"""extend a forward primer to a given length"""
+def forwardExtend(DNA, primer, length):
+    return
 
-def reverseExtend(DNA, primer, length): """extend a reverse primer to a given length"""
+"""extend a reverse primer to a given length"""
+def reverseExtend(DNA, primer, length):
+    return
 
 
-"""program"""
+#"""program"""
 
-n = 2000 """length of original template"""
-m = 200 """length of segment to amplify"""
-p = 20 """length of primers"""
+n = 2000 #"""length of original template"""
+m = 200 #"""length of segment to amplify"""
+p = 20 #"""length of primers"""
 
-strands = [] """collection of all dna strands in the simulation"""
+template = generateStrand(n) # original dna strand 
+strands = [template, getComplement(template)] #"""collection of all dna strands in the simulation"""
+
