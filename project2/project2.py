e24544686eca080e48300ebda5d001eab736dff8
tl <thl13@zips.uakron.edu>
2019-03-04 14:42:08 -0500

    Some basic planning

---
+++ project2/project2.py
@@ -0,0 +1,13 @@
+"""Data Structures"""
+
+"""Helper Functions"""
+
+# BLASTN algorithm
+# Given a query and target sequence or nucleotide bases,
+# a scoring matrix, query length, and score cutoff:
+# (1) Identify and filter out low-complexity regions
+# (2) Create a list of sufficiently-scoring sub-queries
+# (3) Search for matches of each query in the target sequence
+# (4)
+
+"""Driver Program"""
